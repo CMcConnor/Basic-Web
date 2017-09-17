0e77c56496ffdc05efc699e46ea844513def4bd6
Connor McCrory <mccro016@umn.edu>
2017-09-17 17:00:31 -0500

    adding jquery to filter pictures

--- scripts/local.js
+++ scripts/local.js
@@ -1,3 +1,43 @@
 $(function() {
+  $("#cat-button").click(function(){
+    console.log("Cat");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","#ffc082");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","");
+    $( ".columns" ).find(".has-dog").hide();
+    $( ".columns" ).find(".has-giraffe").hide();
+    $( ".columns" ).find(".has-cat").show();
+  })
 
+  $("#dog-button").click(function(){
+    console.log("Dog");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","#ffc082");
+    $("#giraffe-button").css("background-color","");
+    $( ".columns" ).find(".has-cat").hide();
+    $( ".columns" ).find(".has-giraffe").hide();
+    $( ".columns" ).find(".has-dog").show();
+  })
+
+  $("#giraffe-button").click(function(){
+    console.log("Giraffe");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","#ffc082");
+    $( ".columns" ).find(".has-cat").hide();
+    $( ".columns" ).find(".has-dog").hide();
+    $( ".columns" ).find(".has-giraffe").show();
+  })
+
+  $("#all-button").click(function(){
+    console.log("All");
+    $("#all-button").css("background-color","#ffc082");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","");
+    $(".columns").find(".column").show();
+  })
 });
