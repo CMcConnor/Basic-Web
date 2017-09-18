ed2f8707961e079caee5d6443f03ab79bd8e4f33
Connor McCrory <mccro016@umn.edu>
2017-09-17 21:50:32 -0500

    adding color to buttons

--- scripts/local.js
+++ scripts/local.js
@@ -1,10 +1,15 @@
 $(function() {
+  $("#all-button").css("background-color","#ffc082");
   $("#cat-button").click(function(){
     console.log("Cat");
     $("#all-button").removeClass("is-active");
     $("#cat-button").addClass("is-active");
     $("#dog-button").removeClass("is-active");
     $("#giraffe-button").removeClass("is-active");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","#ffc082");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","");
     $( ".columns" ).find(".has-dog").hide();
     $( ".columns" ).find(".has-giraffe").hide();
     $( ".columns" ).find(".has-cat").show();
@@ -16,6 +21,10 @@ $(function() {
     $("#cat-button").removeClass("is-active");
     $("#dog-button").addClass("is-active");
     $("#giraffe-button").removeClass("is-active");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","#ffc082");
+    $("#giraffe-button").css("background-color","");
     $( ".columns" ).find(".has-cat").hide();
     $( ".columns" ).find(".has-giraffe").hide();
     $( ".columns" ).find(".has-dog").show();
@@ -27,6 +36,10 @@ $(function() {
     $("#cat-button").removeClass("is-active");
     $("#dog-button").removeClass("is-active");
     $("#giraffe-button").addClass("is-active");
+    $("#all-button").css("background-color","");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","#ffc082");
     $( ".columns" ).find(".has-cat").hide();
     $( ".columns" ).find(".has-dog").hide();
     $( ".columns" ).find(".has-giraffe").show();
@@ -38,6 +51,10 @@ $(function() {
     $("#cat-button").removeClass("is-active");
     $("#dog-button").removeClass("is-active");
     $("#giraffe-button").removeClass("is-active");
+    $("#all-button").css("background-color","#ffc082");
+    $("#cat-button").css("background-color","");
+    $("#dog-button").css("background-color","");
+    $("#giraffe-button").css("background-color","");
     $(".columns").find(".column").show();
   })
 });
