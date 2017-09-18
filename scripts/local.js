b708a0d3260a84c73c5c78c14bc36607b15f619d
Connor McCrory <mccro016@umn.edu>
2017-09-17 19:23:19 -0500

    putting in is-active class for buttons

--- scripts/local.js
+++ scripts/local.js
@@ -1,10 +1,10 @@
 $(function() {
   $("#cat-button").click(function(){
     console.log("Cat");
-    $("#all-button").css("background-color","");
-    $("#cat-button").css("background-color","#ffc082");
-    $("#dog-button").css("background-color","");
-    $("#giraffe-button").css("background-color","");
+    $("#all-button").removeClass("is-active");
+    $("#cat-button").addClass("is-active");
+    $("#dog-button").removeClass("is-active");
+    $("#giraffe-button").removeClass("is-active");
     $( ".columns" ).find(".has-dog").hide();
     $( ".columns" ).find(".has-giraffe").hide();
     $( ".columns" ).find(".has-cat").show();
@@ -12,10 +12,10 @@ $(function() {
 
   $("#dog-button").click(function(){
     console.log("Dog");
-    $("#all-button").css("background-color","");
-    $("#cat-button").css("background-color","");
-    $("#dog-button").css("background-color","#ffc082");
-    $("#giraffe-button").css("background-color","");
+    $("#all-button").removeClass("is-active");
+    $("#cat-button").removeClass("is-active");
+    $("#dog-button").addClass("is-active");
+    $("#giraffe-button").removeClass("is-active");
     $( ".columns" ).find(".has-cat").hide();
     $( ".columns" ).find(".has-giraffe").hide();
     $( ".columns" ).find(".has-dog").show();
@@ -23,10 +23,10 @@ $(function() {
 
   $("#giraffe-button").click(function(){
     console.log("Giraffe");
-    $("#all-button").css("background-color","");
-    $("#cat-button").css("background-color","");
-    $("#dog-button").css("background-color","");
-    $("#giraffe-button").css("background-color","#ffc082");
+    $("#all-button").removeClass("is-active");
+    $("#cat-button").removeClass("is-active");
+    $("#dog-button").removeClass("is-active");
+    $("#giraffe-button").addClass("is-active");
     $( ".columns" ).find(".has-cat").hide();
     $( ".columns" ).find(".has-dog").hide();
     $( ".columns" ).find(".has-giraffe").show();
@@ -34,10 +34,10 @@ $(function() {
 
   $("#all-button").click(function(){
     console.log("All");
-    $("#all-button").css("background-color","#ffc082");
-    $("#cat-button").css("background-color","");
-    $("#dog-button").css("background-color","");
-    $("#giraffe-button").css("background-color","");
+    $("#all-button").addClass("is-active");
+    $("#cat-button").removeClass("is-active");
+    $("#dog-button").removeClass("is-active");
+    $("#giraffe-button").removeClass("is-active");
     $(".columns").find(".column").show();
   })
 });
